2df43c13dd8edaaa8ae6866c7b0f46ac503cd4b4
Ryan Zhiyang Tan <ryatan@c2r2s6.42singapore.sg>
2025-12-04 13:27:33 +0800

    init

---
+++ get_next_line.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ryatan <ryatan@student.42singapore.sg      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/03 13:26:51 by ryatan            #+#    #+#             */
+/*   Updated: 2025/12/03 23:46:49 by ryatan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+}
+
+//int	main(void)
+//{
+//	//char	buffer[100000];
+//	int	fd;
+//
+//	fd = open("testfile.txt", O_RDONLY);
+//	if (fd == -1)
+//	{
+//		printf("failed to open file.\n");
+//		return (-1);
+//	}
+//	printf("file opened successfully!\n");
+//	return (0);
+//}
---
+++ get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ryatan <ryatan@student.42singapore.sg      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/03 13:26:58 by ryatan            #+#    #+#             */
+/*   Updated: 2025/12/03 13:44:59 by ryatan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <fcntl.h>
+
+// testing lib
+# include <stdio.h>
+
+char	*get_next_line(int fd);
+
+#endif
